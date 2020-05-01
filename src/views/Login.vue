1e4f32420b6b4f2eea25c5754b22727e1dc3d26b
vinny neves <vinicios_neves@hotmail.com>
2020-05-01 20:05:47 -0300

    fim da aula 2.2

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
     <h1>Login</h1>
-    <form>
+    <form @submit.prevent="efetuarLogin">
       <div class="form-group">
         <label for="email">E-mail</label>
         <input type="email" class="form-control" v-model="usuario.email" />
       </div>
       <div class="form-group">
         <label for="senha">Senha</label>
-        <input type="password" class="form-control" v-model="usuario.password" />
+        <input type="password" class="form-control" v-model="usuario.senha" />
       </div>
       <button type="submit" class="btn btn-primary brn-block">Logar</button>
       <router-link :to="{ name: 'novo.usuario' }">Não possui um cadastro, cadastre-se aqui!</router-link>
@@ -17,11 +17,25 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       usuario: {}
     };
+  },
+  methods: {
+    efetuarLogin() {
+      axios
+        .post("http://localhost:8000/auth/login", this.usuario)
+        .then(response => {
+          console.log(response);
+          localStorage.setItem("token", response.data.access_token);
+          this.$router.push({ name: "gerentes" });
+        })
+        .catch(erro => console.log(erro));
+    }
   }
 };
 </script>
