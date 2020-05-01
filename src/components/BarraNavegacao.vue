984dda007f41aefa7c7f461593cba72941f3453f
vinny neves <vinicios_neves@hotmail.com>
2020-05-01 20:21:04 -0300

    fim da aula 3.1

---
+++ src/components/BarraNavegacao.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-bytebank">
+    <a class="navbar-brand" href="#">ByteBank</a>
+    <button class="navbar-toggler" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+      <BarraNavegacaoQuandoLogado v-if="usuarioestaLogado" />
+      <BarraNavegacaoQuandoDeslogado v-else />
+    </div>
+  </nav>
+</template>
+
+<script>
+import BarraNavegacaoQuandoLogado from "./BarraNavegacaoQuandoLogado";
+import BarraNavegacaoQuandoDeslogado from "./BarraNavegacaoQuandoDeslogado";
+
+export default {
+  components: {
+    BarraNavegacaoQuandoLogado,
+    BarraNavegacaoQuandoDeslogado
+  },
+  computed: {
+    usuarioestaLogado() {
+      return Boolean(localStorage.getItem("token"));
+    }
+  }
+};
+</script>
+<style>
+.navbar {
+  background: #27ae60;
+}
+.navbar-bytebank a {
+  color: #fff;
+}
+.navbar-bytebank a:hover {
+  color: #000;
+}
+</style>
---
+++ src/components/BarraNavegacaoQuandoDeslogado.vue
@@ -0,0 +1,10 @@
+<template>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <router-link class="nav-link" to="/login">Login</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/cadastre-se" class="nav-link">Registre-se</router-link>
+    </li>
+  </ul>
+</template>
