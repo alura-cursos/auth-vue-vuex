0c8fb4abbced4f15ea92011d1e694197bc5b7c05
vinny neves <vinicios_neves@hotmail.com>
2020-05-01 19:58:15 -0300

    fim da aula 2.1

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Gerentes from '../views/Gerentes.vue'
 import Home from '../views/Home.vue'
 import NovoUsuario from '../views/NovoUsuario.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -21,8 +22,12 @@ const routes = [
     path: '/cadastre-se',
     name: 'novo.usuario',
     component: NovoUsuario
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
-
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <form>
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="email" class="form-control" v-model="usuario.email" />
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.password" />
+      </div>
+      <button type="submit" class="btn btn-primary brn-block">Logar</button>
+      <router-link :to="{ name: 'novo.usuario' }">Não possui um cadastro, cadastre-se aqui!</router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: {}
+    };
+  }
+};
+</script>
