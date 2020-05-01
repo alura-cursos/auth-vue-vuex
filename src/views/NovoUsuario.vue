f9a3b515376994eb24410684a9d8625c48265a62
vinny neves <vinicios_neves@hotmail.com>
2020-05-01 19:38:50 -0300

    fim da aula 1.3

--- src/views/NovoUsuario.vue
+++ src/views/NovoUsuario.vue
@@ -1,3 +1,41 @@
  <template>
-    <h1>Novo usuário</h1>
+    <div class="container">
+        <h1>Novo usuário</h1>
+        <form @submit.prevent="enviarFormulario">
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" v-model="usuario.nome">
+            </div>
+            <div class="form-group">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control" v-model="usuario.email">
+            </div>
+            <div class="form-group">
+                <label for="senha">Senha</label>
+                <input type="password" class="form-control" v-model="usuario.senha">
+            </div>
+            <button class="btn btn-primary" type="submit">Salvar</button>
+        </form>
+    </div>
  </template>
+ 
+ <script>
+export default {
+
+    data: function () {
+        return {
+            usuario: {
+                nome: '',
+                senha: '',
+                email: ''
+            }
+        }
+    },
+    methods: {
+        enviarFormulario () {
+            console.log(this.usuario)
+        }
+    }
+
+}
+</script>
