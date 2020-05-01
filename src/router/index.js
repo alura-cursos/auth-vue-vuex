d8811af6016499fec640b582488a9ddc65733585
vinny neves <vinicios_neves@hotmail.com>
2020-05-01 20:44:29 -0300

    fim da aula 4.3

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Gerentes from '../views/Gerentes.vue'
 import Home from '../views/Home.vue'
-import NovoUsuario from '../views/NovoUsuario.vue'
 import Login from '../views/Login.vue'
 import provedor from '../provedor'
 
@@ -22,7 +21,7 @@ const routes = [
   {
     path: '/cadastre-se',
     name: 'novo.usuario',
-    component: NovoUsuario,
+    component: () => import(/* webpackChunkName: "registrar" */ '../views/NovoUsuario.vue'),
     meta: {
       publica: true
     }
